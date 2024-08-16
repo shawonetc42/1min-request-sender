c2ed6f3d676d517c12d56f680120bc0cf4fdead9
shawonetc42 <139037447+shawonetc42@users.noreply.github.com>
2024-08-16 22:24:13 +0600

    Update mon.py

--- mon.py
+++ mon.py
@@ -1,13 +1,25 @@
 import requests
 import time
+from requests.exceptions import RequestException
 
+# সাইটের URL
 url = "https://testtmon.onrender.com/test"
 
-while True:
+def send_request():
     try:
-        response = requests.get(url)
-        print(f"Status Code: {response.status_code}, Content: {response.text}")
-    except requests.exceptions.RequestException as e:
+        response = requests.get(url, timeout=10)  # টাইমআউট ১০ সেকেন্ডে সেট করা হয়েছে
+        response.raise_for_status()  # যদি স্ট্যাটাস কোড ২০০ না হয় তাহলে এ্যারর তুলবে
+        print(f"Status Code: {response.status_code}, Content: {response.json()}")
+    except RequestException as e:
         print(f"An error occurred: {e}")
-    
-    time.sleep(60)  # ১ মিনিট অপেক্ষা করুন
+        # ৫ সেকেন্ড পরে পুনরায় চেষ্টা করবে
+        time.sleep(5)
+        send_request()
+
+def main():
+    while True:
+        send_request()
+        time.sleep(60)  # ১ মিনিট পর পুনরায় রিকুয়েস্ট পাঠাবে
+
+if __name__ == "__main__":
+    main()
